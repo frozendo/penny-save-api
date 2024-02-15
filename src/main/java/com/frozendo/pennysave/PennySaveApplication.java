e03b0d1b5449e8c7e843466a6621bf14bd47269b
Flavio Rozendo <s2it_frozendo@uolinc.com>
2024-02-15 18:55:02 -0300

    Start project

---
+++ src/main/java/com/frozendo/pennysave/PennySaveApplication.java
@@ -0,0 +1,13 @@
+package com.frozendo.pennysave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennySaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennySaveApplication.class, args);
+	}
+
+}
