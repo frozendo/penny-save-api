37fef6bae95c992f39fcd40d63cd16f11e8e33b7
Flavio Rozendo <s2it_frozendo@uolinc.com>
2024-02-21 18:31:26 -0300

    Config docker and integration tests environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,7 @@ subprojects {
 		implementation("org.springframework.boot:spring-boot-starter-web")
 		runtimeOnly("org.postgresql:postgresql")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("org.springframework.boot:spring-boot-testcontainers")
-		testImplementation("org.testcontainers:junit-jupiter")
-		testImplementation("org.testcontainers:postgresql")
+		testImplementation(testFixtures(project(":penny-save-test")))
 	}
 
 	tasks.withType<Test> {
